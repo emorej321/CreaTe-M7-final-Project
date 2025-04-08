fa968e603784171467c32bf2d542a4d8d8ff6da7
janni2310 <jannikboettner@gmail.com>
2025-04-08 18:09:04 +0200

    Pipfile setup

--- Model.py
+++ Model.py
@@ -1,9 +1,8 @@
 import pandas as pd
-#import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
-from ucimlrepo import fetch_ucirepo
+
 
 # fetch dataset
 def eye_opener():
